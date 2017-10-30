c90646d875b6ddee58da8f429f763a4daad45a9d
Aleš Kopecký <kopecky@flowhills.com>
2017-10-30 12:54:25 +0100

    add generated asp.net api project

---
+++ Backend/API/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using Castle.Windsor;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace API
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        private readonly IWindsorContainer container = new WindsorContainer();
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            BootstrapContainer();
+        }
+        public IWindsorContainer Container
+        {
+            get { return container; }
+        }
+
+        private void BootstrapContainer()
+        {
+            container.Install(new WebApiInstaller());
+
+            GlobalConfiguration.Configuration.Services
+                .Replace(typeof(IHttpControllerActivator), new WindsorCompositionRoot(container));
+        }
+    }
+}
